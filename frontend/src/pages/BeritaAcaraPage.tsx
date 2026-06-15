9c5714c2509623f9fac6711c12eaaaaf1ebae464
Evan Pasaribu <pasaribu.evann@gmail.com>
2026-06-15 19:49:37 +0700

    fix: syntax error in BeritaAcaraPage print preview map closure

--- frontend/src/pages/BeritaAcaraPage.tsx
+++ frontend/src/pages/BeritaAcaraPage.tsx
@@ -521,7 +521,7 @@ export default function BeritaAcaraPage() {
                                             <td style={printTd}>{item.note || '-'}</td>
                                         </tr>
                                         );
-                                    }))
+                                    })}
                                 </tbody>
                             </table>
 
